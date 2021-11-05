2ac9712b9cb04311d668437d0a33519bd89758e5
jbargu <jure.grabnar@gmail.com>
2021-11-05 11:09:36 +0100

    Expose MAX_GAS_LIMIT, MAX_STORAGE_LIMIT

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './Signer';
 export * from './Provider';
 export * from './SigningKey';
 export * from './TestAccountSigningKey';
+export { U64MAX as MAX_GAS_LIMIT } from './utils';
+export { U32MAX as MAX_STORAGE_LIMIT } from './utils';
